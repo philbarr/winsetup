e4bf0c2a3bb22e10daa0f96c1743e2a58483ca99
Phil Barr <philbarrmail@gmail.com>
2014-10-18 01:49:24 +0100

    adfs

--- chocolately.bat
+++ chocolately.bat
@@ -1,1 +1,1 @@
-START http://boxstarter.org/package/url?https://gist.githubusercontent.com/philbarr/adb97e580b0c6b3690a0/raw/7bf8815221e918890bc4ddf332e92329704a0106/gistfile1.txt
+START http://boxstarter.org/package/url?https://raw.githubusercontent.com/philbarr/winsetup/master/install_list.txt
